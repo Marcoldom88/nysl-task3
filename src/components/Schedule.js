5d5a65ba07daa2c7629437c841b6a0f4fb5e4e2c
Marcoldom88 <marcoldom88@gmail.com>
2022-12-05 17:44:31 +0100

    updated schedule page with new design
    removed App, code added instead to index.js

--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Match } from './Match';
+import '../stylesheets/Schedule.css'
 
 
 export const Schedule = () => {
@@ -15,15 +16,32 @@ export const Schedule = () => {
     fetchGames();
   }, []);
 
-  const Title = ({matches})=> (
-    <div>
-    { Object.values(matches).map(match => <Match key={match.id} match={ match } />) }
-    </div>
+  const CreateGamesTable = ({ matches }) => (
+  
+  <div className="games-container">
+        <table className="games-table table">
+          <thead>
+            <tr>
+              <th colSpan={3}><h3>Upcoming Games</h3></th>
+            </tr>
+            <tr>
+              <th>Teams</th>
+              <th>Date</th>
+              <th>Time</th>
+            </tr>
+          </thead>
+          <tbody>
+            { Object.values(matches).map(match => <Match key={match.id} match={ match } />) }
+          </tbody>
+        </table>
+      </div>
+    
   );
   if (!games) return <h1>Loading schedule...</h1>;
 
   return (
-      <Title matches={games.matches} />
+    <CreateGamesTable matches={games.matches} />
+         
     )
   
 }; 
